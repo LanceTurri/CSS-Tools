dab8de3688b78ce33a9917fff71fd06611edf189
Lance Turri <lance.turri@gmail.com>
2016-06-05 12:14:05 -0500

    Converted js files to typescript, added typing references, updated variables to ES6 syntax.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,50 @@
 'use strict';
 
 var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var debug = require('gulp-debug')
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var ext_replace = require('gulp-ext-replace');
 var pngquant = require('imagemin-pngquant');
+var sourcemaps = require('gulp-sourcemaps');
+var typescript = require('gulp-typescript');
 
-gulp.task('sass', function () {
-  return gulp.src('css/*.scss')
+var isDebugEnabled = true;
+
+gulp.task('styles', function () {
+  return gulp.src('styles/*.scss')
     .pipe(sass())
+    .pipe(gulpif(isDebugEnabled, debug({ title: 'CSS |' })))
     .on("error", notify.onError(function (error) {
         return "File: " + error.message;
       }))
-    .pipe(gulp.dest('css'));
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('scss'))
+    .pipe(notify("SCSS task finished"));
 });
+
+gulp.task('scripts', function() {
+  return gulp.src(['scripts/*.ts'])
+    .pipe(sourcemaps.init())
+    .pipe(typescript({
+      noImplicitAny: false,
+      out: 'output.js'
+    }))
+    .pipe(gulp.dest('scripts'))
+    .pipe(gulpif(isDebugEnabled, debug({ title: 'JS |' })))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(ext_replace('.min.js'))
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('scripts'))
+    .pipe(notify("JS task finished"));
+})
 
 gulp.task('images', function() {
     return gulp.src('images/art/*')
@@ -24,6 +55,9 @@ gulp.task('images', function() {
         .pipe(gulp.dest('images'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('css/*.scss', ['sass']);
+gulp.task('watch', function () {
+  gulp.watch('styles/*.scss', ['styles']);
+  gulp.watch('scripts/*.ts', ['scripts']);
 });
+
+gulp.task('default', ['watch']);
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,58 +1,67 @@
+/// <reference path="../typings/globals/knockout/index.d.ts" />
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+
 // =============================================================================
 // VIEWMODEL FUNCTIONS
 // =============================================================================
-function hexToRgb(hex) {
+interface ColorArray {
+    r: number;
+    g: number;
+    b: number;
+}
+
+function hexToRgb(hex: string): ColorArray  {
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    let shorthandRegex: RegExp = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b): string {
         return r + r + g + g + b + b;
     });
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    let result: RegExpExecArray = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
         r: parseInt(result[1], 16),
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
     } : null;
-};
+}
 
-function rgbToHex(r, g, b) {
+function rgbToHex(r: number, g: number, b: number): string {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
+}
 
-function createVendorPrefixes(cssString, cssPropertyName, prefixArray) {
-        // This will build the code to be put in the <pre> element.
-        var property = 'box-shadow';
-        var prefixes = [
-            '-moz-' + cssPropertyName + ': ',
-            '-webkit-' + cssPropertyName + ': ',
-            cssPropertyName + ': '
-        ]
-        var prefixesLength = prefixes.length;
-        var stringBuilder = '';
-        var i = 0;
+function createVendorPrefixes(cssString: string, cssPropertyName: string, prefixArray?: string[]): string {
+    // This will build the code to be put in the <pre> element.
+    let property: string = 'box-shadow';
+    let prefixes: string[] = [
+        '-moz-' + cssPropertyName + ': ',
+        '-webkit-' + cssPropertyName + ': ',
+        cssPropertyName + ': '
+    ];
+    let prefixesLength: number = prefixes.length;
+    let stringBuilder: string = '';
+    let i: number = 0;
+    
+    if ( prefixArray ) {
+        prefixes = prefixArray;
+    }
+    
+    for ( i; i < prefixesLength; i++ ) {
+        stringBuilder += prefixes[i];
+        stringBuilder += cssString + ';';
         
-        if ( prefixArray ) {
-            prefixes = prefixArray;
+        if ( i < prefixesLength - 1 ) {
+            stringBuilder += '\n';
         }
-        
-        for ( i; i < prefixesLength; i++ ) {
-            stringBuilder += prefixes[i];
-            stringBuilder += cssString + ';';
-            
-            if ( i < prefixesLength - 1 ) {
-                stringBuilder += '\n';
-            }
-        }
-        
-        return stringBuilder;
+    }
+    
+    return stringBuilder;
 }
 
 // =============================================================================
 // VIEWMODELS
 // =============================================================================
-var appViewModel = function() {
-    var self = this;
+let appViewModel = function(): void {
+    const self = this;
 
     self.panels = ko.observable({
         "box-shadow": new boxShadowViewModel(),
@@ -60,22 +69,24 @@ var appViewModel = function() {
         "rgb-to-hex": new rgbToHexViewModel()
     });
 
+    // Set default to box-shadow
     self.activeTab = ko.observable({
         name: 'box-shadow',
         data: ko.observable(self.panels()['box-shadow'])
     });
 
-    self.changeActiveTab = function(name) {
+    self.changeActiveTab = function(name: string): void {
         console.log(name);
-        self.activeTab()['name'] = name;
-        self.activeTab()['data'](self.panels()[name]);
+        self.activeTab().name = name;
+        self.activeTab().data(self.panels()[name]);
 
         console.log(self.activeTab());
     }
 };
 
-var boxShadowViewModel = function() {
-    var self = this;
+let boxShadowViewModel = function(): void {
+    const self = this;
+    
     self.horizontalLength = ko.observable(10);
     self.verticalLength = ko.observable(10);
     self.blurRadius = ko.observable(5);
@@ -91,7 +102,7 @@ var boxShadowViewModel = function() {
     self.opacity = ko.observable(0.75);
     self.outlineOrInset = ko.observable('outline');
 
-    self.colorBuilder = ko.computed(function() {
+    self.colorBuilder = ko.computed(function(): string {
     	if (self.colorType() === 'HEX') {
     		return self.shadowColor();
     	} else if (self.colorType() === 'RGBA') {
@@ -101,14 +112,14 @@ var boxShadowViewModel = function() {
     	}
     });
 
-    self.boxShadowBuilder = ko.computed(function() {
-        var horizontalLength = self.horizontalLength() + 'px ';
-        var verticalLength = self.verticalLength() + 'px ';
-        var blurRadius = self.blurRadius() + 'px ';
-        var spreadRadius = self.spreadRadius() + 'px ';
-        var colorBuilder = self.colorBuilder();
+    self.boxShadowBuilder = ko.computed(function(): string {
+        let horizontalLength: string = self.horizontalLength() + 'px ';
+        let verticalLength: string = self.verticalLength() + 'px ';
+        let blurRadius: string = self.blurRadius() + 'px ';
+        let spreadRadius: string = self.spreadRadius() + 'px ';
+        let colorBuilder: string = self.colorBuilder();
         
-        var boxShadowString = '';
+        let boxShadowString: string = '';
         
         // Example string: '-18px 10px 5px 0px #000000'
         boxShadowString += horizontalLength;
@@ -124,8 +135,8 @@ var boxShadowViewModel = function() {
     });
 };
 
-var borderRadiusViewModel = function() {
-	var self = this;
+let borderRadiusViewModel = function(): void {
+	const self = this;
 
 	self.radiusAll = ko.observable(50);
 	self.radiusTopLeft = ko.observable(50);
@@ -145,13 +156,13 @@ var borderRadiusViewModel = function() {
     self.borderColor = ko.observable('#cccccc');
     self.borderRadius = ko.observable('');
 
-    self.borderRadiusBuilder = ko.computed(function() {
-        var topLeft = self.radiusTopLeft() + 'px ';
-        var topRight = self.radiusTopRight() + 'px ';
-        var bottomRight = self.radiusBottomRight() + 'px ';
-        var bottomLeft = self.radiusBottomLeft() + 'px';
+    self.borderRadiusBuilder = ko.computed(function(): string {
+        let topLeft: string = self.radiusTopLeft() + 'px ';
+        let topRight: string = self.radiusTopRight() + 'px ';
+        let bottomRight: string = self.radiusBottomRight() + 'px ';
+        let bottomLeft: string = self.radiusBottomLeft() + 'px';
         
-        var borderRadiusString = '';
+        let borderRadiusString: string = '';
         
         borderRadiusString += topLeft;
         borderRadiusString += topRight;
@@ -164,15 +175,14 @@ var borderRadiusViewModel = function() {
     });
 }
 
-var rgbToHexViewModel = function() {
-    var self = this;
+let rgbToHexViewModel = function(): void {
+    const self = this;
 
     self.activeInputHex = null;
     
-    self.hexColor = ko.observable('');
-    self.hexColor.extend({ rateLimit: 100 });
-    self.hexColor.subscribe(function(newValue) {
-        var hexLength = newValue.replace('#', '').length;
+    self.hexColor = ko.observable('').extend({ rateLimit: 100 });
+    self.hexColor.subscribe(function(newValue: string) {
+        let hexLength: number = newValue.replace('#', '').length;
         
         if ( (hexLength === 3 || hexLength === 6) && self.activeInputHex ) {
             self.hexConvert(newValue);
@@ -182,11 +192,10 @@ var rgbToHexViewModel = function() {
         self.activeInputHex = true;
     });
     
-    self.rgbColor = ko.observable('');
-    self.rgbColor.extend({ rateLimit: 100 });
-    self.rgbColor.subscribe(function(newValue) {
-        var rgbTemp = newValue.replace('(', '').replace(')', '').split(',');
-        var rgbLength = rgbTemp.length;
+    self.rgbColor = ko.observable('').extend({ rateLimit: 100 });
+    self.rgbColor.subscribe(function(newValue: string) {
+        let rgbTemp: string[] = newValue.replace('(', '').replace(')', '').split(',');
+        let rgbLength: number = rgbTemp.length;
         
         if ( rgbLength === 3 && !self.activeInputHex ) {
             self.rgbConvert(rgbTemp);
@@ -195,25 +204,26 @@ var rgbToHexViewModel = function() {
         self.activeInputHex = false;
     });
     
-    self.rgbConvert = function(rgbArray) {
-        var rgbString = rgbToHex(parseInt(rgbArray[0]), parseInt(rgbArray[1]), parseInt(rgbArray[2]));
+    self.rgbConvert = function(rgbArray: any): void {
+        let rgbString: string = rgbToHex(parseInt(rgbArray[0]), parseInt(rgbArray[1]), parseInt(rgbArray[2]));
         
         self.hexColor(rgbString);
     };
     
-    self.hexConvert = function(hexColor) {
-        var rgbArray = hexToRgb(hexColor);
-        var rgbFormatted = [
+    self.hexConvert = function(hexColor: string): void {
+        let rgbArray: ColorArray = hexToRgb(hexColor);
+        let rgbFormatted: any = [
             rgbArray.b,
             rgbArray.g,
             rgbArray.r
         ];
-        var rgbString = '(' + rgbFormatted.join(', ') + ')';
+        let rgbString: string = '(' + rgbFormatted.join(', ') + ')';
         
         self.rgbColor(rgbString);
     };
 };
 
 $(function() {
-    ko.applyBindings(new appViewModel());
+    const viewModel: void = new appViewModel();
+    ko.applyBindings(viewModel);
 });
