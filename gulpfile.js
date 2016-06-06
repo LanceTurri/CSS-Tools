527dc87586ecf7c7c1fb3dc5c7c376e6401b9f2b
Lance Turri <lance.turri@gmail.com>
2016-06-05 22:35:17 -0500

    Minor updates to structure of gulpfile and removed some unnecessary dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,13 @@ var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var debug = require('gulp-debug')
 var sass = require('gulp-sass');
+var merge = require('merge2');
 var notify = require('gulp-notify');
-var imagemin = require('gulp-imagemin');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var ext_replace = require('gulp-ext-replace');
-var pngquant = require('imagemin-pngquant');
 var sourcemaps = require('gulp-sourcemaps');
 var typescript = require('gulp-typescript');
-var tslint = require('gulp-tslint');
 
 var isDebugEnabled = true;
 
@@ -29,40 +27,30 @@ gulp.task('styles', function () {
     .pipe(notify("SCSS task finished"));
 });
 
-gulp.task('typescript', ['scripts'], function() {
-  return gulp.src(['scripts/*.ts'])
+gulp.task('scripts', function() {
+  var tsResult = gulp.src(['scripts/*.ts'])
     .pipe(typescript({
       noImplicitAny: true,
       outFile: 'main.js'
     }))
-    .pipe(gulp.dest('scripts'));
-});
-
-gulp.task('scripts', function() {
-  var tsResult = gulp.src(['scripts/*.js', '!scripts/*.min.js'])
-    .pipe(sourcemaps.init())
-    .pipe(gulpif(isDebugEnabled, debug({ title: 'JS |' })))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(uglify())
-    .pipe(ext_replace('.min.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('scripts'))
-    .pipe(notify("JS task finished"))    
+    
+  merge([
+    tsResult.js.pipe(sourcemaps.init())
+      .pipe(gulpif(isDebugEnabled, debug({ title: 'JS |' })))
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(uglify())
+      .pipe(ext_replace('.min.js'))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('scripts'))
+      .pipe(notify("JS task finished")),
+    tsResult.js.pipe(gulp.dest('scripts')) 
+  ])
 })
-
-gulp.task('images', function() {
-    return gulp.src('images/art/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-        }))
-        .pipe(gulp.dest('images'));
-});
 
 gulp.task('watch', function () {
   gulp.watch('styles/*.scss', ['styles']);
-  gulp.watch('scripts/*.ts', ['typescript']);
+  gulp.watch('scripts/*.ts', ['scripts']);
 });
 
 gulp.task('default', ['watch']);
